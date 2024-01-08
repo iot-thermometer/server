3c4cc5c4cfe752f4f6b72b5095db211e18730647
Mateusz Woźniak <matisiek11@gmail.com>
2024-01-08 09:36:41 +0100

    fix: remove unique from alert name

--- internal/model/alert.go
+++ internal/model/alert.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Alert struct {
 	gorm.Model
-	AlertName       string  `gorm:"type:varchar(100);not null;unique" json:"alert_name"`
+	AlertName       string  `gorm:"type:varchar(100);not null" json:"alert_name"`
 	UserID          uint    `gorm:"not null" json:"user_id"`
 	DeviceID        uint    `gorm:"not null" json:"device_id"`
 	SoilMoistureMin float64 `gorm:"not null" json:"soil_moisture_min"`
