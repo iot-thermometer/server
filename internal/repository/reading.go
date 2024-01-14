f09fd4b1394df0220ed0196e4364e498133b89eb
Mateusz Woźniak <matisiek11@gmail.com>
2024-01-14 23:06:35 +0100

    fix: order by

--- internal/repository/reading.go
+++ internal/repository/reading.go
@@ -24,7 +24,7 @@ func newReadingRepository(db *gorm.DB) Reading {
 
 func (r reading) List(deviceID uint, start, end time.Time) ([]model.Reading, error) {
 	readings := make([]model.Reading, 0)
-	if err := r.db.Where("device_id = ? and created_at > ? and created_at < ?", deviceID, start, end).Find(&readings).Error; err != nil {
+	if err := r.db.Where("device_id = ? and measured_at > ? and measured_at < ?", deviceID, start, end).Order("measured_at DESC").Limit(100).Find(&readings).Error; err != nil {
 		return nil, err
 	}
 	return readings, nil
